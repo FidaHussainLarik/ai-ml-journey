cb1fc275c3de9c9b5c28952829f5d1fdc6f5628b
FidaHussain110 <fidahussainsthp23@gmail.com>
2026-06-29 15:51:54 +0500

    Replaced the original code file which was pushed as a blank file

--- 01_python-fundamentals/02_count-down-timer/CountDownTimer.py
+++ 01_python-fundamentals/02_count-down-timer/CountDownTimer.py
@@ -1,1 +1,16 @@
 
+import time
+
+
+print("============================================================")
+
+print("     This is a mini-project called COUNTDOWN TIMER          ")
+
+print("============================================================")
+
+counter = 10
+while counter != 1:
+    print(counter)
+    time.sleep(1) # This will allow each next iteration to wait for one second.
+    counter = counter-1
+print("\nHappy new year! 🎉🎊🥂\n")
